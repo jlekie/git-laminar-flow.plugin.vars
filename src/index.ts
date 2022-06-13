e7ed096f332a2909bb0666ec05652b50ff7f4668
Joe Lekie <joe.lekie@umbrexus.com>
2022-06-13 08:10:21 -0500

    checkpoint

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as Path from 'path';
+import * as FS from 'fs-extra';
+import * as Zod from 'zod';
+
+import { PluginHandler } from '@jlekie/git-laminar-flow-cli';
+
+const OptionsSchema = Zod.object({
+    varsFilePath: Zod.string()
+});
+
+const createPlugin: PluginHandler = (options) => {
+    const parsedOptions = OptionsSchema.parse(options);
+
+    return {
+        updateVersion: async (oldVersion, newVersion, { config, stdout, dryRun }) => {
+            // const versionRegex = new RegExp(`VERSION=${oldVersion ?? '(.*)'}$`, 'gm');
+            const versionRegex = new RegExp(`^VERSION=(.*)$`, 'gm');
+            const varsFilePath = Path.resolve(config.path, parsedOptions.varsFilePath);
+
+            await FS.ensureFile(varsFilePath);
+
+            let content = await FS.readFile(varsFilePath, 'utf8');
+            if (versionRegex.test(content))
+                content = content.replace(versionRegex, `VERSION=${newVersion}`);
+            else
+                content += `${!content.length || content.endsWith('\n') ? '' : '\n'}VERSION=${newVersion}\n`;
+
+            if (!dryRun) {
+                await FS.writeFile(varsFilePath, content, 'utf8');
+                stdout?.write(`Updated vars file written to ${varsFilePath}\n`);
+            }
+        }
+    }
+}
+
+export default createPlugin;
