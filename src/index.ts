552816f2581104f50477bebf475351c2a7230d09
Joe Lekie <joe.lekie@umbrexus.com>
2022-09-20 15:55:38 -0500

    peer support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import * as Path from 'path';
 import * as FS from 'fs-extra';
+import * as Bluebird from 'bluebird';
 import * as Zod from 'zod';
 
 import { PluginHandler } from '@jlekie/git-laminar-flow-cli';
 
 const OptionsSchema = Zod.object({
     varsFilePath: Zod.string(),
+    peers: Zod.object({
+        identifier: Zod.string(),
+        variable: Zod.string()
+    }).array().optional()
 });
 
 const createPlugin: PluginHandler = (options) => {
@@ -39,7 +44,7 @@ const createPlugin: PluginHandler = (options) => {
             const versionRegex = new RegExp(`^VERSION=(.*)$`, 'gm');
             const varsFilePath = Path.resolve(config.path, parsedOptions.varsFilePath);
 
-            await FS.ensureFile(varsFilePath);
+            !dryRun && await FS.ensureFile(varsFilePath);
 
             let content = await FS.readFile(varsFilePath, 'utf8');
             if (versionRegex.test(content))
@@ -50,6 +55,32 @@ const createPlugin: PluginHandler = (options) => {
             if (!dryRun) {
                 await FS.writeFile(varsFilePath, content, 'utf8');
                 stdout?.write(`Updated vars file written to ${varsFilePath}\n`);
+            }
+
+            if (parsedOptions.peers) {
+                await Bluebird.map(parsedOptions.peers, async peer => {
+                    const peerIntegration = config.parentConfig?.submodules.find(s => s.config.identifier === peer.identifier)?.config.integrations.find(i => i.plugin === '@jlekie/git-laminar-flow-plugin-vars');
+
+                    if (peerIntegration) {
+                        const parsedOptions = OptionsSchema.parse(peerIntegration.options);
+
+                        const versionRegex = new RegExp(`^${peer.variable}=(.*)$`, 'gm');
+                        const varsFilePath = Path.resolve(peerIntegration.parentConfig.path, parsedOptions.varsFilePath);
+
+                        !dryRun && await FS.ensureFile(varsFilePath);
+
+                        let content = await FS.readFile(varsFilePath, 'utf8');
+                        if (versionRegex.test(content))
+                            content = content.replace(versionRegex, `${peer.variable}=${newVersion}`);
+                        else
+                            content += `${!content.length || content.endsWith('\n') ? '' : '\n'}${peer.variable}=${newVersion}\n`;
+
+                        if (!dryRun) {
+                            await FS.writeFile(varsFilePath, content, 'utf8');
+                            stdout?.write(`Updated peer vars file written to ${varsFilePath}\n`);
+                        }
+                    }
+                });
             }
         }
     }
